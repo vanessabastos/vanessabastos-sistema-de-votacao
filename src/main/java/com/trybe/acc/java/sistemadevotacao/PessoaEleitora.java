782c7c794c05e45af706a9d6445d3b4eede0cc95
vanessabastos <vanessarsb83@gmail.com>
2023-01-21 22:14:00 -0300

    Implementa a Classe PessoaEleitora.

--- src/main/java/com/trybe/acc/java/sistemadevotacao/PessoaEleitora.java
+++ src/main/java/com/trybe/acc/java/sistemadevotacao/PessoaEleitora.java
@@ -1,5 +1,28 @@
 package com.trybe.acc.java.sistemadevotacao;
 
-public class PessoaEleitora {
+/**Classe Pessoa Eleitora.*/
 
+public class PessoaEleitora extends Pessoa {
+  private String cpf;
+
+  public PessoaEleitora(String nome, String cpf) {
+    super.nome = nome;
+    this.cpf = cpf;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
 }
